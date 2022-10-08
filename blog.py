f6bea70217bb89d10330c7983989f7e3669fe418
gftree <30354652+gftree@users.noreply.github.com>
2022-10-09 00:31:24 +0100

    x

--- blog.py
+++ blog.py
@@ -9,9 +9,9 @@ openai.api_key = config.OPENAI_API_KEY
 def generateBlogTopics(prompt1):
     response = openai.Completion.create(
       engine="davinci-instruct-beta-v3",
-      prompt="Generate blog topics on: {}. \n \n 1.  ".format(prompt1),
-      temperature=0.7,
-      max_tokens=100,
+      prompt="List the names of the best places for tourists to visit in: {} \n\n - ".format(prompt1),
+      temperature=0.1,
+      max_tokens=300,
       top_p=1,
       frequency_penalty=0,
       presence_penalty=0
@@ -22,9 +22,9 @@ def generateBlogTopics(prompt1):
 def generateBlogSections(prompt1):
     response = openai.Completion.create(
       engine="davinci-instruct-beta-v3",
-      prompt="Expand the blog title in to high level blog sections: {} \n\n- Introduction: ".format(prompt1),
-      temperature=0.6,
-      max_tokens=100,
+      prompt="Write a factual overview of: {}".format(prompt1),
+      temperature=0.1,
+      max_tokens=300,
       top_p=1,
       frequency_penalty=0,
       presence_penalty=0
@@ -37,8 +37,8 @@ def blogSectionExpander(prompt1):
     response = openai.Completion.create(
       engine="davinci-instruct-beta-v3",
       prompt="Expand the blog section in to a detailed professional , witty and clever explanation.\n\n {}".format(prompt1),
-      temperature=0.7,
-      max_tokens=200,
+      temperature=0.1,
+      max_tokens=600,
       top_p=1,
       frequency_penalty=0,
       presence_penalty=0
